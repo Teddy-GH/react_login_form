8185e55bab6122cab7584de96cdcd089d2621b4d
TeddyG <teddygirma20@gmail.com>
2024-09-09 11:18:10 +0300

    protected route

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
